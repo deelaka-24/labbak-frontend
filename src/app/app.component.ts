39c4160e69dcb3b5f4610d610e1b7383c790cf99
Deelaka Ransilu <88099140+deelaka-24@users.noreply.github.com>
2025-04-16 23:54:51 +0530

    added tailwind, flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'labbak-frontend';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
